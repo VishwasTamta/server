10f73990c44c6aab4b2858d7e4149b5e11105697
Vishwas Tamta <vishwastamta@gmail.com>
2023-02-10 16:52:46 +0530

    defined new relations for cart and cartItems

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const errorController = require("./controllers/error");
 const sequelize = require("./utils/database");
 const Product = require("./models/product");
 const User = require("./models/user");
+const Cart = require("./models/cart");
+const CartItem = require("./models/cart-item");
 
 const app = express();
 
@@ -32,11 +34,15 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
-  // .sync({ force: true }) // remove for development
+  .sync({ force: true }) // remove for development
   // make tables according to the models defined
-  .sync()
+  // .sync()
   .then((result) => {
     return User.findByPk(1);
   })
