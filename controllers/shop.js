cba2669322ed8ddd924bd1d589faa88964df11f8
Vishwas Tamta <vishwastamta@gmail.com>
2023-02-15 23:10:27 +0530

    added pdfkit for invoice generetion

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,8 @@
+const fs = require("fs");
+const path = require("path");
 const Product = require("../models/product");
 const Order = require("../models/order");
+const PDFDocument = require("pdfkit");
 
 exports.getProducts = (req, res, next) => {
   Product.find()
@@ -135,6 +138,56 @@ exports.getOrders = (req, res, next) => {
         pageTitle: "Your Orders",
         orders: orders,
       });
+    })
+    .catch((err) => {
+      const error = new Error(err);
+      err.httpStatusCode(500);
+      return next(error);
+    });
+};
+
+exports.getInvoice = (req, res, next) => {
+  const orderId = req.params.orderId;
+
+  Order.findById(orderId)
+    .then((order) => {
+      if (!order) {
+        return next(new Error("No order found!"));
+      }
+      if (order.user.userId.toString() !== req.user._id.toString()) {
+        return next(new Error("Unauthorized"));
+      }
+
+      const invoiceName = "invoice-" + orderId + ".pdf";
+      const invoicePath = path.join("data", "invoices", invoiceName);
+
+      const pdfDoc = new PDFDocument();
+      res.setHeader("Content-Type", "application/pdf");
+      res.setHeader(
+        "Content-Disposition",
+        'inline; filename="' + invoiceName + '"'
+      );
+      pdfDoc.pipe(fs.createWriteStream(invoicePath));
+      pdfDoc.pipe(res);
+      pdfDoc.fontSize(26).text("Invoice", { underline: true });
+      pdfDoc.text("--------------------------");
+      let totalprice = 0;
+      order.products.forEach((prod) => {
+        totalprice += prod.quantity * prod.product.price;
+        pdfDoc
+          .fontSize(12)
+          .text(
+            prod.product.title +
+              " - " +
+              prod.quantity +
+              " x " +
+              "Rs." +
+              prod.product.price
+          );
+      });
+      pdfDoc.text("-------------------------");
+      pdfDoc.fontSize(20).text("Total Price: " + "Rs." + totalprice);
+      pdfDoc.end();
     })
     .catch((err) => {
       const error = new Error(err);
