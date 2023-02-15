c0e4c7766acc9cb1c17ffee66f333218e2a0d0cb
Vishwas Tamta <vishwastamta@gmail.com>
2023-02-16 02:27:49 +0530

    added checkout page

--- controllers/shop.js
+++ controllers/shop.js
@@ -227,3 +227,27 @@ exports.getInvoice = (req, res, next) => {
       return next(error);
     });
 };
+
+exports.getCheckout = (req, res, next) => {
+  req.user
+    .populate("cart.items.productId")
+    .execPopulate()
+    .then((user) => {
+      const products = user.cart.items;
+      let total = 0;
+      products.forEach((p) => {
+        total += p.quantity * p.productId.price;
+      });
+      res.render("shop/checkout", {
+        path: "/checkout",
+        pageTitle: "Checkout",
+        products: products,
+        totalSum: total,
+      });
+    })
+    .catch((err) => {
+      const error = new Error(err);
+      err.httpStatusCode(500);
+      return next(error);
+    });
+};
